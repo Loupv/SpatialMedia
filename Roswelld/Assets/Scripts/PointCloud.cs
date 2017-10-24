59f40391f836757d5d9c91be02684adfc4d649e3
Loup Vuarnesson <loup@vuarnesson.com>
2017-10-24 16:55:20 +0200

    - maj for git - point cloud shader v1

---
+++ Roswelld/Assets/Scripts/PointCloud.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+public class PointCloud : MonoBehaviour
+{
+
+	public Mesh mesh, previousMesh;
+	//int numPoints = 60000;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+		mesh = GetComponent<MeshFilter> ().mesh;
+		mesh.SetIndices (mesh.GetIndices (0), MeshTopology.Points, 0);
+	}
+
+
+}
