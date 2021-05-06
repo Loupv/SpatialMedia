c4745234d531c6d170948100c1ccc1cc4f2323eb
dionysiz <dzamplaras@gmail.com>
2021-05-06 16:11:29 +0200

    ajout scripts video et camera

---
+++ ToolboxAndAssets/Scripts/SoundFromMicrophone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundFromMicrophone : MonoBehaviour
+{
+
+    public string selectedDevice { get; private set; }
+
+    public int device = 1;
+
+    private int m_device;
+
+    private AudioSource audioSource;
+    // Start recording with built-in Microphone and play the recorded audio right away
+    void Start()
+    {
+        m_device = device;
+        MicroLauncher(device);
+    }
+
+    void Update()
+    {
+        if (device != m_device)
+        {
+            MicroLauncher(device);
+            m_device = device;
+
+        }
+
+    }
+
+    void MicroLauncher(int j)
+    {
+        audioSource = GetComponent<AudioSource>();
+        selectedDevice = Microphone.devices[j].ToString();
+        Debug.Log(selectedDevice);
+        audioSource.clip = Microphone.Start(Microphone.devices[j], true, 1, 44100);
+        audioSource.Play();
+
+    }
+
+}
---
+++ ToolboxAndAssets/Scripts/VideoFromCamera.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoFromCamera : MonoBehaviour
+{
+
+    WebCamTexture webcamTexture;
+
+    public int device;
+
+    private int m_device;
+
+    void Start()
+    {
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for (int i = 0; i < devices.Length; i++)
+        {
+            Debug.Log(devices[i].name);
+        }
+        CameraLauncher(device);
+        m_device = device;
+
+    }
+
+
+
+    void Update()
+    {
+        if (device != m_device)
+        {
+            webcamTexture.Stop();
+            CameraLauncher(device);
+            m_device = device;
+
+        }
+
+    }
+
+
+
+    void CameraLauncher(int j)
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture(devices[j].name);
+        Debug.Log(webcamTexture.deviceName);
+
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        //webcamTexture.Stop();
+        webcamTexture.Play();
+
+    }
+
+
+}
