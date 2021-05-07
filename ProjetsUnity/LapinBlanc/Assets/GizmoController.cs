e28071b37f2674a696124a0f6f2c7e2254be1917
Loup Vuarnesson <loup@vuarnesson.com>
2021-05-07 12:14:20 +0200

    added lapinblanc unity project
    several scenes in project :
    - light multiplayer engine for VR
    - organic generative shapes

---
+++ ProjetsUnity/LapinBlanc/Assets/GizmoController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ [RequireComponent(typeof(MeshCollider))]
+ 
+ public class GizmoController : MonoBehaviour 
+ {
+
+    public bool draggable;
+
+    public Mirror.NetworkIdentity networkIdentity;
+
+
+    private void Start()
+    {
+        networkIdentity = transform.parent.GetComponent<Mirror.NetworkIdentity>();
+    }
+
+    void OnMouseDrag()
+         {
+            if (draggable && networkIdentity.isLocalPlayer)
+            {
+                float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+                Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+                transform.position = new Vector3(pos_move.x, pos_move.y, transform.position.z);
+            }
+         }
+
+
+ 
+ }
---
+++ ProjetsUnity/LapinBlanc/Assets/SyncVar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+
+public class SyncVar : NetworkBehaviour
+{
+
+    [SyncVar(hook = nameof(SetColor))]
+    Color32 _color = Color.red;
+
+    //[SyncVar(hook = nameof(SetPosition))]
+    //Vector3 _position;
+
+    // Start is called before the first frame update
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        //_position = new Vector3(Random.Range(-2f, 2f), 0, 0);
+        StartCoroutine(_RandomizeColor());
+    }
+
+    // Update is called once per frame
+    void SetColor(Color32 oldColor, Color32 newColor)
+    {
+
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        renderer.materials[0].color = newColor;
+
+    }
+
+    /*void SetPosition(Vector3 oldPosition, Vector3 newPosition)
+    {
+        GetComponent<Transform>().position = newPosition;
+    }*/
+
+    private IEnumerator _RandomizeColor()
+    {
+        WaitForSeconds wait = new WaitForSeconds(2f);
+
+        while(true)
+        {
+            yield return wait;
+            _color = Random.ColorHSV(0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f);
+        }
+    }
+
+}
